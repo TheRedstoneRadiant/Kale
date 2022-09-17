7769312828a7f34f6b457078a4198c499357e00c
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2022-09-17 11:41:18 -0400

    Implement zoom scroll

--- public/js/create.js
+++ public/js/create.js
@@ -1,9 +1,18 @@
 const paste = document.getElementById("paste");
+let fontSize = 10;
 
-window.onbeforeunload = function() {
-    if (paste.value) {
-        return true;
-    }
-
-    return null;
+window.onbeforeunload = () => {
+    return paste.value ? true : null;
 };
+
+const zoom = (event) => {
+    if (!event.ctrlKey) return;
+    event.preventDefault();
+
+    fontSize += event.deltaY * -0.01;
+    fontSize = Math.min(Math.max(10, fontSize), 80);
+
+    paste.style.fontSize = `${fontSize}px`;
+}
+
+window.addEventListener('wheel', zoom, {passive: false})
